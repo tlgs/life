01f0e8b1e12b1ebc3a64a102dcb32a8a227cba67
Tiago Seabra <tlgs@users.noreply.github.com>
2020-10-19 19:46:20 +0100

    style: variable renaming

--- static/life.js
+++ static/life.js
@@ -5,26 +5,26 @@ function* neighbors(x) {
     yield x - 100;
     yield x + 100;
     yield x - 101;
+    yield x + 101;
     yield x - 99;
     yield x + 99;
-    yield x + 101;
 }
 
 function tick(board) {
     let calc = new Set(board);
     for (const cell of board) {
-        for (const n of neighbors(cell)) {
-            calc.add(n);
+        for (const neighbor of neighbors(cell)) {
+            calc.add(neighbor);
         }
     }
 
-    let nxt = new Set();
+    let nextBoard = new Set();
     for (const cell of calc) {
-        let alive = [...neighbors(cell)].reduce((a, x) => a + board.has(x), 0)
+        let alive = [...neighbors(cell)].reduce((acc, x) => acc + board.has(x), 0)
         if (alive == 3 || (board.has(cell) && alive == 2)) {
-            nxt.add(cell)
+            nextBoard.add(cell)
         }
     }
 
-    return nxt
+    return nextBoard
 }
