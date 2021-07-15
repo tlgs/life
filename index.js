e6c6922d6470f7beaaf6af60cf41786b041c8e5a
Tiago Seabra <tlgs@users.noreply.github.com>
2021-07-15 17:47:30 +0100

    refactor: eslint

--- index.js
+++ index.js
@@ -10,36 +10,41 @@
  * as such, features such as refresh rate and cell colors are hard-coded.
  */
 
-/** Yield all the neighbors of a cell. */
-function* neighbors(x) {
-    yield x - 1;
-    yield x + 1;
-    yield x - 100;
-    yield x + 100;
-    yield x - 101;
-    yield x + 101;
-    yield x - 99;
-    yield x + 99;
+let board = new Set();
+let prev = board;
+for (let i = 0; i < 1200; i += 1) {
+  board.add(Math.floor(Math.random() * 60) * 100 + Math.floor(Math.random() * 60));
 }
 
-/** Generate the next board state according to the game's rules. */
-function tick(board) {
-    let calc = new Set(board);
-    for (const cell of board) {
-        for (const neighbor of neighbors(cell)) {
-            calc.add(neighbor);
-        }
-    }
+/** Yield all the neighbors of a cell. */
+function* neighbors(x) {
+  yield x - 1;
+  yield x + 1;
+  yield x - 100;
+  yield x + 100;
+  yield x - 101;
+  yield x + 101;
+  yield x - 99;
+  yield x + 99;
+}
 
-    let nextBoard = new Set();
-    for (const cell of calc) {
-        let alive = [...neighbors(cell)].reduce((acc, x) => acc + board.has(x), 0)
-        if (alive == 3 || (board.has(cell) && alive == 2)) {
-            nextBoard.add(cell)
-        }
+function tick(b) {
+  const calc = new Set(b);
+  for (const cell of b) {
+    for (const neighbor of neighbors(cell)) {
+      calc.add(neighbor);
     }
+  }
 
-    return nextBoard
+  const nb = new Set();
+  for (const cell of calc) {
+    const alive = [...neighbors(cell)].reduce((acc, x) => acc + b.has(x), 0);
+    if (alive === 3 || (b.has(cell) && alive === 2)) {
+      nb.add(cell);
+    }
+  }
+
+  return nb;
 }
 
 /**
@@ -47,40 +52,33 @@ function tick(board) {
  * It is the function that is repeatedly called by `window.setInterval()`.
  */
 function draw() {
-    let ctx = document.getElementById('grid').getContext('2d');
+  const ctx = document.getElementById('grid').getContext('2d');
 
-    ctx.clearRect(0, 0, 600, 600);
+  ctx.clearRect(0, 0, 600, 600);
 
-    // draw grid
-    ctx.strokeStyle = 'rgba(0, 0, 0, 0.04)';
-    for (let i = 0; i < 61; i++) {
-        ctx.beginPath();
-        ctx.moveTo(0, i * 10);
-        ctx.lineTo(600, i * 10);
-        ctx.stroke();
+  // draw grid
+  ctx.strokeStyle = 'rgba(0, 0, 0, 0.04)';
+  for (let i = 0; i < 61; i += 1) {
+    ctx.beginPath();
+    ctx.moveTo(0, i * 10);
+    ctx.lineTo(600, i * 10);
+    ctx.stroke();
 
-        ctx.beginPath();
-        ctx.moveTo(i * 10, 0);
-        ctx.lineTo(i * 10, 600);
-        ctx.stroke();
-    }
+    ctx.beginPath();
+    ctx.moveTo(i * 10, 0);
+    ctx.lineTo(i * 10, 600);
+    ctx.stroke();
+  }
 
-    for (const cell of board) {
-        ctx.beginPath();
-        ctx.arc((cell % 100) * 10 + 5, Math.floor(cell / 100) * 10 + 5, 4, 0, 2 * Math.PI);
-        ctx.fillStyle = prev.has(cell) ? 'steelblue' : 'lightsteelblue';
-        ctx.fill();
-    }
+  for (const cell of board) {
+    ctx.beginPath();
+    ctx.arc((cell % 100) * 10 + 5, Math.floor(cell / 100) * 10 + 5, 4, 0, 2 * Math.PI);
+    ctx.fillStyle = prev.has(cell) ? 'steelblue' : 'lightsteelblue';
+    ctx.fill();
+  }
 
-    prev = board;
-    board = tick(board);
-}
-
-
-let board = new Set();
-let prev = board;
-for (let i = 0; i < 1200; i++) {
-    board.add(Math.floor(Math.random() * 60) * 100 + Math.floor(Math.random() * 60));
+  prev = board;
+  board = tick(board);
 }
 
 window.setInterval(draw, 100);
